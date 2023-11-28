e4288a33b38030be5683893d2a674c8502160179
Герман Гашков <herman.gashkov@gmail.com>
2023-11-28 21:52:05 +0500

    Добавил отображение показаний

--- Pages/Apartments/Index.cshtml.cs
+++ Pages/Apartments/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using waterMeter.Models;
 
 namespace waterMeter.Pages.Apartments
@@ -14,8 +15,9 @@ namespace waterMeter.Pages.Apartments
         }
 
         public IList<Meter> Meters { get; set; }
-        public IList<Apartment> Apartments { get;set; }
+        public IList<Apartment> Apartments { get; set; }
         public IList<MetersData> MetersDatas { get; set; }
+        public Dictionary<int, object> ActualDatas = new Dictionary<int, object>();
 
         public async Task OnGetAsync()
         {
@@ -27,6 +29,20 @@ namespace waterMeter.Pages.Apartments
             Meters = await _context.Meter.ToListAsync();
             Apartments = await _context.Apartment.ToListAsync();
             MetersDatas = await _context.MetersData.ToListAsync();
+            CompleteActualData();
+        }
+
+        public void CompleteActualData()
+        {
+            var GroupedData = MetersDatas.GroupBy(s => s.Meter).ToList();
+
+            foreach (var dat in  GroupedData)
+            {
+                var key = dat.Last().Meter.Id;
+                var val = dat.Last().Value.ToString();
+
+                ActualDatas.Add(key, val);
+            }
         }
     }
 }
